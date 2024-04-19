6adc5852ecb2c9de77f1a2a2563d298d35ead9f9
Piotr Cembrowski <losttape@gmail.com>
2024-04-19 16:55:55 +0200

    bug on sharepage with file component fixed temporarly

--- src/components/ui/File.jsx
+++ src/components/ui/File.jsx
@@ -1,16 +1,13 @@
-import React, { Fragment } from 'react'
 import pdfIcon from './../../assets/pdf.png'
 
 function File(name, description) {
   return (
-    <Fragment>
-      <li className="border-2 p-2 w-52">
-            <img src={pdfIcon}/><hr/>
-            <h3 className="mt-4 font-bold">{console.log(name)}</h3><br/>
-            <p>{description}</p><br/>
-            <button className="border bg-red-700 text-white px-2 py-1 rounded-xl">delete</button>
-      </li>
-    </Fragment>
+    <li className="border-2 p-2 w-52">
+      <img src={pdfIcon}/><hr/>
+      <h3 className="mt-4 font-bold">{console.log(name)}</h3><br/>
+      <p>{description}</p><br/>
+      <button className="border bg-red-700 text-white px-2 py-1 rounded-xl">delete</button>
+    </li>
   )
 }
 
--- src/pages/SharePage.jsx
+++ src/pages/SharePage.jsx
@@ -1,5 +1,6 @@
 import { Fragment, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import pdfIcon from './../assets/pdf.png'
 import File from './../components/ui/File'
 
 function SharePage() {
@@ -7,7 +8,7 @@ function SharePage() {
 
   const [ files, setFiles ] = useState([]);
 
-  useEffect(() => {
+  useEffect(()=>{
     async function fetchViews() {
       const response = await fetch(`http://127.0.0.1:5000/files`,{
           method: 'GET',
@@ -22,20 +23,28 @@ function SharePage() {
       }
   
       const { files } = await response.json();
-  
       return setFiles(files);
     }
     fetchViews()
-  }, [ sharePageId ])
-  
+  },[])
 
+  let content = files.map((file) => {
+    <File key={file.id} name={file.name} description={file.description} />
+  })
+  
   return (
     <Fragment>
       <h1>page params:{sharePageId}</h1>
-      {console.log(files)}
       {
-        files.map((file) => {
-          <File key={file.id} name={file.name} description={file.description} />
+        files.map(file => {
+        return ( 
+          <div className="border-2 p-2 w-52">
+            <img src={pdfIcon}/><hr/>
+            <h3 className="mt-4 font-bold">{file.name}</h3><br/>
+            <p>{file.description}</p><br/>
+            <button className="border bg-red-700 text-white px-2 py-1 rounded-xl">delete</button>
+          </div>
+        )
         })
       }
     </Fragment>
