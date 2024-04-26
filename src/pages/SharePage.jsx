2e8edccb8b0b42516407d03a7edc68b3f5873b99
Piotr Cembrowski <losttape@gmail.com>
2024-04-26 19:51:18 +0200

    update of mutate)

--- src/pages/SharePage.jsx
+++ src/pages/SharePage.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import pdfIcon from './../assets/pdf.png'
 import { fetchView } from "./../lib/fetchView";
@@ -9,6 +9,26 @@ function SharePage(list) {
 
   const [ files, setFiles ] = useState([]);
 
+  useEffect(()=>{
+    async function fetchViews() {
+      const response = await fetch(`http://127.0.0.1:5000/files`,{
+          method: 'GET',
+          credentials: 'include',
+      });
+  
+      if (!response.ok) {
+          const error = new Error('An error occured while fetching the events.');
+          error.code = response.status;
+          error.info = await response.json();
+          throw error;
+      }
+  
+      const { files } = await response.json();
+      return setFiles(files);
+    }
+    fetchViews()
+  },[])
+
   const { mutate } = useMutation({
     mutationFn: fetchView,
     onSuccess: () => {
@@ -16,15 +36,20 @@ function SharePage(list) {
     }
   })
 
-  mutate({
-    id: sharePageId,
-    files: files,
-  })
+  useEffect(() =>{
 
-  const { data, isPending, isError, error, refetch} = useQuery({
-    queryKey: ['view'],
-    queryFn: fetchView(sharePageId),
-  });
+    mutate({
+      files
+    })
+
+    console.log(files)
+  },[])
+
+
+  // const { data, isPending, isError, error, refetch} = useQuery({
+  //   queryKey: ['view'],
+  //   queryFn: fetchView(sharePageId),
+  // });
   
   return (
     <Fragment>
