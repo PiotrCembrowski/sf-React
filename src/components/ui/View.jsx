393e4c7a38d4cd76fe1c1223b684ef1773b7adf4
Piotr Cembrowski <losttape@gmail.com>
2024-05-13 12:31:12 +0200

    fixes with share button and fixes with functions

--- src/components/ui/View.jsx
+++ src/components/ui/View.jsx
@@ -1,118 +1,145 @@
-import { useEffect, useState } from 'react';
-import Select from 'react-select'
+import { useEffect, useState } from "react";
+import Select from "react-select";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { fetchFiles } from "./../../lib/fetchFiles";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "./alert-dialog";
-import { Button } from './button'
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./alert-dialog";
+import { Button } from "./button";
 import ErrorBlock from "./ErrorBlock";
-import LoadingIndicator from './LoadingIndicator'
-import { NavLink } from 'react-router-dom';
-import { fetchView } from '../../lib/fetchView';
-import { resultView } from '../../lib/resultView';
-
-
-let listId = 0;
+import LoadingIndicator from "./LoadingIndicator";
+import { NavLink } from "react-router-dom";
+import { fetchView } from "../../lib/fetchView";
+import { resultView } from "../../lib/resultView";
 
 function View() {
+  const [blob, setBlob] = useState([]);
+  const [list, setList] = useState([]);
+  const [link, setLink] = useState("Link to share");
+  const [viewId, setViewId] = useState("");
 
-    const [list, setList] = useState([])
-    const [link, setLink] = useState('Link to share')
-    const [viewId, setViewId] = useState('')
-    
-    let share_button = <AlertDialogAction>Share</AlertDialogAction>;
+  const { data, isPending, isError, error, refetch } = useQuery({
+    queryKey: ["files"],
+    queryFn: fetchFiles,
+  });
 
-    const { data, isPending, isError, error, refetch } = useQuery({
-        queryKey: ['files'],
-        queryFn: fetchFiles,
-    });
+  const { mutate } = useMutation({
+    mutationFn: fetchView,
+    onSuccess: resultView,
+  });
 
-    const { data: fetchedView, mutate } = useMutation({
-        mutationFn: fetchView,
-        onSuccess: resultView
-    })
+  const sendFiles = (array) => {
+    mutate(array);
+  };
 
-    let content = list.map((file) => { return <li key={file.id}>{file.name}</li> });
-    let options = [];
-    data.map(option => options.push({value:option.id, label:option.name}))
+  let share_button = (
+    <button
+      onClick={() => {
+        sendFiles(blob);
+      }}
+    >
+      Share
+    </button>
+  );
 
-    useEffect(() => {
-        share_button = <AlertDialogAction disabled >Share</AlertDialogAction>
-    }, [link]);
+  let content = list.map((file) => {
+    return <li key={file.id}>{file.name}</li>;
+  });
+  let options = [];
+  data.map((option) => options.push({ value: option.id, label: option.name }));
 
-    const stripString = (string) => {
-        console.log(string)
-        const newString = string.replace(/[0-9]/g, "")
-        const updatedString = newString.replace(/-/g, "")
-        return updatedString
+  useEffect(() => {
+    share_button = (
+      <button
+        onClick={() => {
+          sendFiles(blob);
+        }}
+        disabled
+      >
+        Share
+      </button>
+    );
+  }, [link]);
+
+  const stripString = (string) => {
+    console.log(string);
+    const newString = string.replace(/[0-9]/g, "");
+    const updatedString = newString.replace(/-/g, "");
+    return updatedString;
+  };
+
+  const addToList = (e) => {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].name == e.label) {
+        return alert("This file was already added.");
+      }
     }
 
-    const addToList = (e) => {
-        for(let i = 0; i < list.length; i++) {
-            if(list[i].name == e.label) {
-                return alert('This file was already added.')
-            }
+    setList((prevState) => [...prevState, { id: e.value, name: e.label }]);
+    const uuid = crypto.randomUUID();
+    const urlPath = stripString(uuid);
+    const url = `http://127.0.0.1:5000/views/${urlPath}.html`;
+
+    setLink(url);
+    setViewId(uuid);
+  };
+
+  useEffect(() => {
+    setBlob(viewId, list);
+  }, [viewId, list]);
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="Failed to fetch the files."
+        message={
+          error.info?.message ||
+          "Failed to fetch your data. Please try again later."
         }
-        
-        setList(prevState => ([...prevState, { id: e.value, name: e.label}]))
-        const uuid = crypto.randomUUID();
-        const urlPath = stripString(uuid)
-        const url = `http://127.0.0.1:5000/views/${urlPath}.html`;
+      />
+    );
+  }
 
-        setLink(url)
-        setViewId(uuid)
-    }
-
-    useEffect(() => {
-        const blob = []
-        blob.push(viewId, list)
-        mutate(blob)
-    }, [viewId, list])
-
-    if(isPending) {
-        content = <LoadingIndicator/>
-    }
-
-    if(isError) {
-        content = <ErrorBlock title="Failed to fetch the files." message={error.info?.message || "Failed to fetch your data. Please try again later."} />
-    }
-
-    return (
-        <div className="my-2">
-            <AlertDialog>
-            <AlertDialogTrigger asChild>
-                <Button variant="outline">Share</Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-                <AlertDialogHeader>
-                <AlertDialogTitle>Create a new View with files</AlertDialogTitle>
-                <AlertDialogDescription>
-                    Pick files from the list.
-                </AlertDialogDescription>
-                <ul>
-                    {content}
-                </ul>
-                <Select options={options} onChange={(e)=>addToList(e)} />
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                {share_button}
-                </AlertDialogFooter>
-                <br/>
-                <NavLink to={link} target="_blank" rel="noopener noreferrer" >{link}</NavLink>
-            </AlertDialogContent>
-            </AlertDialog>
-        </div>
-    )
+  return (
+    <div className="my-2">
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline">Share</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Create a new View with files</AlertDialogTitle>
+            <AlertDialogDescription>
+              Pick files from the list.
+            </AlertDialogDescription>
+            <ul>{content}</ul>
+            <Select options={options} onChange={(e) => addToList(e)} />
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Close</AlertDialogAction>
+            {share_button}
+          </AlertDialogFooter>
+          <br />
+          <NavLink to={link} target="_blank" rel="noopener noreferrer">
+            {link}
+          </NavLink>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
 }
 
-export default View
+export default View;
