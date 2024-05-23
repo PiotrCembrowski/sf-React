c72a0a702cc24a9d05251373ab4253aa371ddc5a
Piotr Cembrowski <losttape@gmail.com>
2024-05-23 11:57:06 +0200

    cleaning out project

--- src/pages/FilesList.jsx
+++ src/pages/FilesList.jsx
@@ -1,81 +1,97 @@
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { fetchFiles } from "../lib/fetchFiles";
-import LoadingIndicator from './../components/ui/LoadingIndicator';
+import LoadingIndicator from "./../components/ui/LoadingIndicator";
 import ErrorBlock from "../components/ui/ErrorBlock";
 import NewFile from "./NewFile";
-import View from "./../components/ui/View"
-import { deleteFiles } from './../lib/deleteFiles'
-import { queryClient } from './../lib/query_client'
-import pdfIcon from './../assets/pdf.png'
-
+import View from "./../components/ui/View";
+import { deleteFiles } from "./../lib/deleteFiles";
+import { queryClient } from "./../lib/query_client";
+import pdfIcon from "./../assets/pdf.png";
 
 function FilesList(id) {
   const { data, isPending, isError, error, refetch } = useQuery({
-    queryKey: ['files'],
+    queryKey: ["files"],
     queryFn: fetchFiles,
-    staleTime: 500
+    staleTime: 500,
   });
 
   const { mutate } = useMutation({
     mutationFn: deleteFiles,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['files'] });
-    }
-  })
+      queryClient.invalidateQueries({ queryKey: ["files"] });
+    },
+  });
 
   const deleteFileHandler = (id) => {
-    mutate(id)
-  }
+    mutate(id);
+  };
 
   let content;
   let addButton;
   let addView;
 
-  if(isError) {
-    content = <ErrorBlock title="Failed to fetch the filelist." message={error.info?.message || "Failed to fetch your data. Please try again later."} />
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title="Failed to fetch the filelist."
+        message={
+          error.info?.message ||
+          "Failed to fetch your data. Please try again later."
+        }
+      />
+    );
   }
 
-  if(isPending) {
-    content = <LoadingIndicator/>
+  if (isPending) {
+    content = <LoadingIndicator />;
   }
 
-  if(data) {
+  if (data) {
     content = (
       <ul className="my-4">
-        {data.map(file => {
-          if(file.list_id == id.id) return <li key={file.id} className="border-2 p-2 w-52">
-            <img src={pdfIcon}/><hr/>
-            <h3 className="mt-4 font-bold">{file.name}</h3><br/>
-            <p>{file.description}</p><br/>
-            <button className="border bg-red-700 text-white px-2 py-1 rounded-xl" onClick={() => deleteFileHandler(file.id)}>delete</button></li>
-        }
-        )}
+        {data.map((file) => {
+          if (file.list_id == id.id)
+            return (
+              <li key={file.id} className="border-2 p-2 w-52">
+                <img src={pdfIcon} />
+                <hr />
+                <h3 className="mt-4 font-bold">{file.name}</h3>
+                <br />
+                <p>{file.description}</p>
+                <br />
+                <p>{file.url}</p>
+                <br />
+                <button
+                  className="border bg-red-700 text-white px-2 py-1 rounded-xl"
+                  onClick={() => deleteFileHandler(file.id)}
+                >
+                  delete
+                </button>
+              </li>
+            );
+        })}
       </ul>
-    )
+    );
   }
 
-  if(id.id != '') {
-    addButton = (
-      <NewFile pickedListId={id} />
-    )
+  if (id.id != "") {
+    addButton = <NewFile pickedListId={id} />;
   }
 
-  if(id.id != '') {
-    addView = (
-      <View />
-    )
+  if (id.id != "") {
+    addView = <View />;
   }
 
   return (
     <>
       {/* <div>{listName}</div> */}
       {content}
-        <div className="py-12">
-          {addButton}
-          {addView}
-        </div>
+      <div className="py-12">
+        {addButton}
+        {addView}
+      </div>
     </>
-  )
+  );
 }
 
-export default FilesList
+export default FilesList;
--- src/pages/NewFile.jsx
+++ src/pages/NewFile.jsx
@@ -3,101 +3,122 @@ import { useMutation } from "@tanstack/react-query";
 import { uploadFile } from "../lib/uploadFile";
 import { postFile } from "../lib/postFile";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "./../components/ui/alert-dialog";
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./../components/ui/alert-dialog";
 import ErrorBlock from "../components/ui/ErrorBlock";
 import FileUpload from "../components/form/FileUpload";
 import { queryClient } from "../lib/query_client";
 
-function NewFile({pickedListId}) {
-    const [FileNameStore, setFileNameStore] = useState('')
-    const [FileDescriptionStore, setFileDescriptionStore] = useState('')
-    const [attachment, setAttachment] = useState('')
+function NewFile({ pickedListId }) {
+  const [FileNameStore, setFileNameStore] = useState("");
+  const [FileDescriptionStore, setFileDescriptionStore] = useState("");
+  const [attachment, setAttachment] = useState("");
 
-    let content;
+  let content;
 
-    const { mutate: redeemData, isPending, isError, error } = useMutation({
-        mutationFn: postFile,
-        onSuccess: () => {
-            queryClient.invalidateQueries({ queryKey: ['files'] })
-        }
+  const {
+    mutate: redeemData,
+    isPending,
+    isError,
+    error,
+  } = useMutation({
+    mutationFn: postFile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["files"] });
+    },
+  });
+
+  const { mutate: redeemFile } = useMutation({
+    mutationFn: uploadFile,
+  });
+
+  const storeFileNameHandler = (event) => {
+    setFileNameStore(event.target.value);
+  };
+
+  const storeFileDescriptionHandler = (event) => {
+    setFileDescriptionStore(event.target.value);
+  };
+
+  const storeFileAttachmentHandler = (event) => {
+    setAttachment(event.target.files[0]);
+  };
+
+  const addFileHandler = async () => {
+    await redeemFile(attachment);
+
+    await redeemData({
+      name: FileNameStore,
+      description: FileDescriptionStore,
+      list_id: pickedListId.id,
     });
+  };
 
-    const { mutate: redeemFile } = useMutation({
-        mutationFn: uploadFile
-    })
+  content = (
+    <>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction onClick={addFileHandler}>Add file</AlertDialogAction>
+    </>
+  );
 
-    const storeFileNameHandler = (event) => {
-        setFileNameStore(event.target.value)
-    }
+  if (isPending) {
+    return (content = <p>Uploading...</p>);
+  }
 
-    const storeFileDescriptionHandler = (event) => {
-        setFileDescriptionStore(event.target.value)
-    }
+  if (isError) {
+    return (content = (
+      <ErrorBlock
+        title="Failed to add file."
+        message={
+          error.info?.message ||
+          "Failed to upload your file. Please check your inputs and try again."
+        }
+      />
+    ));
+  }
 
-    const storeFileAttachmentHandler = (event) => {
-        setAttachment(event.target.files[0])
-    }
-
-    const addFileHandler = async () => {
-
-        redeemFile(attachment)
-
-        redeemData({
-            "name": FileNameStore,
-            "description": FileDescriptionStore,
-            "list_id": pickedListId.id
-        })
-
-    }
-
-    content = <>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogAction onClick={addFileHandler}>Add file</AlertDialogAction>
-            </>
-
-    if(isPending) {
-        return content = <p>Uploading...</p>;
-    }
-
-    if(isError) {
-        return content = <ErrorBlock title="Failed to add file." message={error.info?.message || "Failed to upload your file. Please check your inputs and try again."} />;
-    }
-    
-    return (
-        <AlertDialog>
-            <AlertDialogTrigger>Add file</AlertDialogTrigger>
-            <AlertDialogContent>
-            <AlertDialogHeader>
-                <form>
-                    <AlertDialogTitle>Set name of the file</AlertDialogTitle>
-                    <p>{FileNameStore}</p>
-                    <AlertDialogDescription>
-                    <input type="text" className="border border-gray-500" required onChange={storeFileNameHandler}/>
-                    </AlertDialogDescription>
-                    <AlertDialogTitle>Description:</AlertDialogTitle>
-                    <AlertDialogDescription>
-                    <textarea type="text" className="border border-gray-500 resize-y" required onChange={storeFileDescriptionHandler}/>
-                    </AlertDialogDescription>
-                    <AlertDialogDescription>
-                        <FileUpload attachFile={storeFileAttachmentHandler}/>
-                    </AlertDialogDescription>
-                </form>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-                {content}
-            </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    )
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>Add file</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <form>
+            <AlertDialogTitle>Set name of the file</AlertDialogTitle>
+            <p>{FileNameStore}</p>
+            <AlertDialogDescription>
+              <input
+                type="text"
+                className="border border-gray-500"
+                required
+                onChange={storeFileNameHandler}
+              />
+            </AlertDialogDescription>
+            <AlertDialogTitle>Description:</AlertDialogTitle>
+            <AlertDialogDescription>
+              <textarea
+                type="text"
+                className="border border-gray-500 resize-y"
+                required
+                onChange={storeFileDescriptionHandler}
+              />
+            </AlertDialogDescription>
+            <AlertDialogDescription>
+              <FileUpload attachFile={storeFileAttachmentHandler} />
+            </AlertDialogDescription>
+          </form>
+        </AlertDialogHeader>
+        <AlertDialogFooter>{content}</AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
 
-export default NewFile
+export default NewFile;
