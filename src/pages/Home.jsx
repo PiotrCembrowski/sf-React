e9b199f335cbc0e2801bb438a315084ed201441b
Piotr Cembrowski <losttape@gmail.com>
2024-05-28 13:26:41 +0200

    new css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
 function Home() {
   return (
     <>
-        <h1 className='text-center text-5xl font-bold mt-32'>Sharefiles</h1>
+      <h1 className="text-center text-5xl font-bold mt-32">Sharefiles</h1>
+      <h3 className="text-center text-2xl">
+        - Effortless File Sharing for Everyone
+      </h3>
+      <div className="mt-12 w-11/12 md:w-3/4 lg:w-2/5 mx-auto text-xl">
+        <p className="text-center">
+          Welcome to ShareFiles, your go-to web app for file sharing. Whether
+          you need to send important documents, share high-resolution images, or
+          collaborate on large projects, ShareFiles makes it fast, easy, and
+          worry-free. No more complicated setups or size limitations—just
+          simple, intuitive file sharing at your fingertips.
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -1,9 +1,10 @@
-
-
 function Pricing() {
-    return (
-      <div>Pricing</div>
-    )
-  }
-  
-  export default Pricing
+  return (
+    <div className="mt-24">
+      <h1 className="text-center text-5xl font-bold">Pricing</h1>
+      <h3 className="text-center text-3xl">Free for testing</h3>
+    </div>
+  );
+}
+
+export default Pricing;
