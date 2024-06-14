f02ce3a4ed463dabea7989c4e2352c01a586871c
Piotr Cembrowski <losttape@gmail.com>
2024-06-14 15:22:53 +0200

    changes in hero section

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,16 +1,15 @@
-import { Outlet } from "react-router-dom"
-import MainNavigation from "./MainNavigation"
-
+import { Outlet } from "react-router-dom";
+import MainNavigation from "./MainNavigation";
 
 function RootLayout() {
   return (
     <>
       <MainNavigation />
-      <main>
-          <Outlet />
+      <main className="w-[1440px]">
+        <Outlet />
       </main>
     </>
-  )
+  );
 }
 
-export default RootLayout
+export default RootLayout;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 function Home() {
   return (
-    <>
-      <h1 className="text-center text-5xl font-bold mt-32">Sharefiles</h1>
-      <h3 className="text-center text-2xl">
+    <div className="w-1/2">
+      <h1 className="text-5xl font-bold mt-32 mb-2 text-gray-600">
+        Sharefiles
+      </h1>
+      <h3 className="text-2xl text-gray-500">
         - Effortless File Sharing for Everyone
       </h3>
-      <div className="mt-12 w-11/12 md:w-3/4 lg:w-2/5 mx-auto text-xl">
-        <p className="text-center">
+      <div className="mt-4 w-11/12 md:w-3/4 lg:w-full text-xl">
+        <p className="text-gray-800">
           Welcome to ShareFiles, your go-to web app for file sharing. Whether
           you need to send important documents, share high-resolution images, or
           collaborate on large projects, ShareFiles makes it fast, easy, and
@@ -14,26 +16,26 @@ function Home() {
           simple, intuitive file sharing at your fingertips.
         </p>
       </div>
-      <div className="w-11/12 md:w-3/4 lg:w-2/5 mx-auto mt-12">
+      <div className="w-11/12 md:w-3/4 lg:w-4/5 mt-8 ml-4">
         <ul className="list-disc">
-          <li className="my-4">
-            <b>User-Friendly Interface</b>: Share files with just a few clicks.
-            No tech skills required.
+          <li className="my-3 text-gray-500">
+            <b className="text-gray-600">User-Friendly Interface</b>: Share
+            files with just a few clicks. No tech skills required.
           </li>
-          <li className="my-4">
-            <b>Safe and easy</b> signing up.
+          <li className="my-3 text-gray-500">
+            <b className="text-gray-600">Safe and easy</b> signing up.
           </li>
-          <li className="my-4">
-            <b>Lightning Fast</b>: Experience rapid uploads and downloads,
-            regardless of file size.
+          <li className="my-3 text-gray-500">
+            <b className="text-gray-600">Lightning Fast</b>: Experience rapid
+            uploads and downloads, regardless of file size.
           </li>
-          <li className="my-4">
-            <b>Cross-Platform</b>: Compatible with all devices and operating
-            systems. Share files anytime, anywhere.
+          <li className="my-3 text-gray-500">
+            <b className="text-gray-600">Cross-Platform</b>: Compatible with all
+            devices and operating systems. Share files anytime, anywhere.
           </li>
         </ul>
       </div>
-    </>
+    </div>
   );
 }
 
